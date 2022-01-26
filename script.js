40628ee7b64006d8f5be9d2a3a332c1b34718564
JHokum <jdelgado92@hotmail.com>
2022-01-26 15:29:24 -0800

    Began work on single round play.  Created helper function to decide winner of round. Rock case is functional. Adding Scissors and Paper case.

--- script.js
+++ script.js
@@ -3,21 +3,44 @@
 //function => computerPlay (This is the computer making a selection)
 //Return Rock, Paper, Or Scissors
 
+
+
 function computerPlay(){
     let selection =Math.floor(Math.random()*3+1);
 
     switch (selection){
         case (1):
-            return "Rock";
+            return "rock";
         case (2):
-            return "Paper";
+            return "paper";
         case (3):
-            return "Scissors";
+            return "scissors";
     }
 };
 
+//testing helper function
+function decider(player, computer){
+    switch(player){
+        case("rock"):{
+            if(computer=="rock"){
+                return "TIE";
+            }
+            else{
+              return  (computer=="scissors") ? "Player Wins!" : "Computer Wins!";
+            }
+        }
+    }
+}
+
 
 //Play single round
+function singleRound(playerSelection, computerSelection){
+    let a = playerSelection;
+    let b = computerSelection;
+
+
+}
+
 //Paramters: playerSelection, computerSelection
 //return string, winner of round
 //selection is case insensitive
