e88e54f2938e641efa77477c854814fbacc6e342
JHokum <jdelgado92@hotmail.com>
2022-01-26 15:13:14 -0800

    Script commented, computerPlay function given randomizer for computer selection

---
+++ script.js
@@ -0,0 +1,17 @@
+//Game will play against computer
+
+//function => computerPlay (This is the computer making a selection)
+//Return Rock, Paper, Or Scissors
+
+function computerPlay(){
+    let selection =Math.floor(Math.random()*3+1);
+}
+
+//Play single round
+//Paramters: playerSelection, computerSelection
+//return string, winner of round
+//selection is case insensitive
+
+//game () = > 5 round game
+//keeps score
+//reports winner or loser
